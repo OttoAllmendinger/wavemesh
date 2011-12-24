464f2ea869f3753f4d60f7c7756dc9fe14830992
Otto Allmendinger <otto.allmendinger@googlemail.com>
2011-12-25 00:56:01 +0100

    interface work

--- src/PoolModel.h
+++ src/PoolModel.h
@@ -1,13 +1,17 @@
 #ifndef POOLMODEL_H
 #define POOLMODEL_H
 
-
 class Particle;
 
 class PoolModel {
   public:
     PoolModel(int width, int height);
     ~PoolModel();
+
+    double* getHeightMap() const;
+
+    int getWidth() const;
+    int getHeight() const;
 
     void update();
 
